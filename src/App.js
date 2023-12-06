b44d4e09c102529a336cd24a1d95ff8cbbf51991
juanImsys <juan.encizo@imsys.com.co>
2023-12-06 16:59:29 -0500

    comit 1 nuevo inicio

--- src/App.js
+++ src/App.js
@@ -1,80 +1,19 @@
 
 
-import { CreateTodoButton } from './CreateTodoButton';
-import { TodoCounter } from './TodoCounter';
-import { TodoItem } from './TodoItem';
-import { TodoList } from './TodoList';
-import { TodoSearch } from './TodoSearch';
+
 import React from 'react';
 
-const defaultTodos = [
-  {text: 'Cortar cebola', completed: true},
-  {text: 'Tomar el curso de intro a react.js', completed: false},
-  {text: 'Llorar con la llorona', completed: true},
-  
-  
-];
 
 
 function App() {
 
-  const [todos, setTodos] = React.useState(defaultTodos);
-
-  const [searchValue, setSearchValue] = React.useState('');
-
-  const completedTodos = todos.filter(todo => !!todo.completed).length;
-  const totalTodos = todos.length;
-
-
-  const searchedTodos = todos.filter(
-    (todo) =>{
-      const todoText = todo.text.toLowerCase();
-      const searchText = searchValue.toLowerCase();
-
-      return todoText.includes(searchText);
-    }
-  );
-
-  const completeTodo = (text) =>{
-    const newTodos = [...todos];
-    const todoIndex  = newTodos.findIndex(
-      (todo) => todo.text == text
-    );
-    newTodos[todoIndex].completed = true;
-    setTodos(newTodos)
-  };
-
-  const deleteTodo = (text) =>{
-    const newTodos = [...todos];
-    const todoIndex  = newTodos.findIndex(
-      (todo) => todo.text == text
-    );
-    newTodos.splice(todoIndex, 1);
-    setTodos(newTodos)
-  };
-
   return (
     <>
+    <span>
+      nuevo2
+    </span>
 
-      <TodoCounter completed={completedTodos}  total={totalTodos}/>
-      <TodoSearch
-       searchValue = {searchValue}
-       setSearchValue = {setSearchValue}
-      />
-
-      <TodoList>
-        {searchedTodos.map(todo => 
-        <TodoItem 
-          key ={todo.text} 
-          text = {todo.text}
-          completed = {todo.completed}
-          onComplete = {() => completeTodo(todo.text)}
-          onDelete = {() => deleteTodo(todo.text)}
-
-        />)}
-      </TodoList>
-
-      <CreateTodoButton/>
+      <p>nuevo</p>
 
     </>
   );
